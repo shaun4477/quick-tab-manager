5e014200ea1199f24eff82c2fa3bf52b2d9580f9
Shaun Clowes <shaun4477@gmail.com>
2018-04-12 10:02:41 -0700

    New command to close duplicate tabs

--- welcome.js
+++ welcome.js
@@ -5,6 +5,29 @@
         return false;
     }
 
-    for (let link of document.querySelectorAll("a.forceUrl")) 
-        link.addEventListener("click", forceLocalUrl);
+    // Take forceUrl class anchors and use chrome.tabs to 
+    // open the location specified in the data-url attribute
+    document.body.addEventListener("click", (event) => {
+        if (event.target.matches("a.forceUrl")) 
+            return forceLocalUrl.call(event.target);
+    });
+
+    var tabPermissions = {permissions: ["tabs"]};
+
+    function updateCheckbox() {
+        chrome.permissions.contains(tabPermissions, (enabled) => {
+            document.querySelector("#tabsPermission").checked = enabled;
+        });
+    }
+
+    updateCheckbox();
+
+    document.querySelector("#tabsPermission").addEventListener("change", (event) => {
+        var checkbox = event.target;
+
+        if (checkbox.checked) 
+            chrome.permissions.request(tabPermissions, updateCheckbox);
+        else
+            chrome.permissions.remove(tabPermissions, updateCheckbox);
+    });
 })(window);
